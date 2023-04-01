c61b9b2528397ff02077ca0d45696bbf9b3d7049
Efe Karakanlı <102619516+actuallyzefe@users.noreply.github.com>
2023-04-01 19:27:30 +0300

    deleted

--- interfaces/SchemaInterfaces.ts
+++
@@ -1,9 +0,0 @@
-import { Types } from 'mongoose';
-
-export interface IUser {
-  _id: Types.ObjectId;
-  name: string;
-  email: string;
-  password: string;
-  comparePassword(userPass: string, hashedPass: string): Promise<boolean>;
-}
